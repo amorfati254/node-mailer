bd28bcbbcde4fe5f6d0d379993643f2bb6060b63
amorfati254 <amorfati254@gmail.com>
2022-10-06 18:17:59 +0300

    created a express server that uses node mailer to send email when requested

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const { main } = require("./mail");
+
+const app = express();
+const corsOption = {
+	origin: "https://premiermart.ml",
+};
+app.use(cors(corsOption));
+app.use(express.json());
+
+app.post("/email", async (req, res) => {
+	const { name, number, orderItems, total } = req.body;
+	console.log("OrderItmes from req.body: ", orderItems);
+	try {
+		const info = await main({ name, number, orderItems, total });
+		console.log("Info from main function", info);
+		res.status(200).json({ success: "email sent" });
+	} catch (error) {
+		res.status(400).json({ error: "authentication failed" });
+		console.log(error);
+	}
+});
+
+app.listen(process.env.PORT, () => {
+	console.log("Listening to port", process.env.PORT);
+});
